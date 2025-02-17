66152503aa422181b9add8dd714cc87466e71ff7
Rama Dhaniansyah <71095453+dhaniansyahr@users.noreply.github.com>
2025-02-17 23:23:21 +0700

    Fix/surat keterangan kuliah (#20)
    * feat: add surat keterangan kuliah (#1)
    
    * Feat/surat keterangan kuliah (#2)
    
    * feat: add surat keterangan kuliah
    
    * feat: add cetak surat
    
    * fix: update workflows
    
    * feat: acl (#3)
    
    * feat: acl
    
    * fix: remove user utils
    
    * feat: cuti sementara (#4)
    
    * Feat/cuti sementara (#6)
    
    * feat: cuti sementara
    
    * fix: remove migration file in git ignore
    
    * Feat/cuti sementara (#8)
    
    * feat: cuti sementara
    
    * fix: remove migration file in git ignore
    
    * fix: update login
    
    * fix: some logic for user, userlevels, and routes
    
    * fix: surat keterangan kuliah
    
    * fix: surat keterangan kuliah (#11)
    
    * fix: surat keterangan kuliah (#12)
    
    * fix: update surat keterangan kuliah
    
    * Fix/surat keterangan kuliah (#13)
    
    * fix: surat keterangan kuliah
    
    * fix: update surat keterangan kuliah
    
    * fix: update logic and verification cuti sementara (#14)
    
    * fix: update verification status in surat keterangan kuliah and cuti sementara
    
    * fix: add field rejectedReason in cuti sementara and logic for rejected
    
    * fix: update status entities surat keterangan kuliah
    
    * feat: add seeder surat keterangan kuliah and cuti sementar
    
    * fix: remove unused vars

--- prisma/seeds/seedCutiSementara.ts
+++ prisma/seeds/seedCutiSementara.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, TypeForSuratKeteranganKuliah } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { ulid } from "ulid";
 
 export async function seedCutiSementara(prisma: PrismaClient) {
