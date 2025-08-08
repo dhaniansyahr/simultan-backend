9a4b01f8b3bc4ca10afc0c170fe5b02cbba6a243
alifannaufallyatha <athaalifan@gmail.com>
2025-08-09 01:28:24 +0700

    Create migration.sql

---
+++ prisma/migrations/20250808074535_deletetranskipijazahonketeranganlulus/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dokumenIjazah` on the `suratketeranganlulus` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `suratketeranganlulus` DROP COLUMN `dokumenIjazah`;
